bec20a93880ee8142276e263b2dca3e82808c51c
Chris Dehner <topherdehner@gmail.com>
2021-01-03 02:57:03 -0500

    Feat: Apply conditional formatting on partial match of word type

--- basic/src/_style.js
+++ basic/src/_style.js
@@ -1,32 +1,45 @@
-let styleTypes = {
+const itemTypes = ["noun", "verb", "adjective", "particle", "counter", "expression", "adverb"];
+const styleTypes = {
 	noun: {
-		mainColor: "#2497E6", //blue
-		darkColor: "#016ea6"  //dark blue
+		mainColor: "#2497E6", // blue
+		darkColor: "#016ea6" // dark blue
 	},
 	verb: {
-		mainColor: "#FA2445",  //red
-		darkColor: "#B82E3F"  //dark red
+		mainColor: "#FA2445", // red
+		darkColor: "#B82E3F" // dark red
 	},
 	adjective: {
-		mainColor: "#FEE071",  //yellow
-		darkColor: "#ACA57C"  //dark yellow
+		mainColor: "#FEE071", // yellow
+		darkColor: "#ACA57C" // dark yellow
 	}
-}
+};
 function styleCard(type) {
-	let style = styleTypes[type];
-	let root = document.querySelector(':root');
-	root.style.setProperty("--main-color",style.mainColor);
+	const style = styleTypes[type];
+	const root = document.querySelector(":root");
+	root.style.setProperty("--main-color", style.mainColor);
 	root.style.setProperty("--dark-color", style.darkColor);
 }
 function wordType() {
-	var typeField = document.querySelector(".wordtypeJS").innerHTML;
-	var regex = /\(([^)]+)\)/;
-	var type = regex.exec(typeField)[1];
-	return type;
+	const typeField = document.querySelector(".wordtypeJS").innerHTML;
+	const regex = /\(([^)]+)\)/;
+	const type = regex.exec(typeField)[1];
+	if (itemTypes.indexOf(type) >= 0) {
+		return type;
+	} else {
+		for (let i = 0; i<itemTypes.length; i++)
+		{
+			if (type.includes(itemTypes[i]))
+			{
+				return itemTypes[i];
+			}
+		}
+	}
+	return "";
 }
 function main() {
-	if ( ["noun", "verb", "adjective"].indexOf(wordType()) >= 0 ) {
-		styleCard(wordType());
+	var type = wordType();
+	if (type) {
+		styleCard(type);
 	}
 }
 window.addEventListener("onchange", main());
