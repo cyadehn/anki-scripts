858d11a417644acfd8926deb10c8ca6be170923b
Chris Dehner <topherdehner@gmail.com>
2021-01-02 22:42:05 -0500

    Comment script file

--- basic/_generate.sh
+++ basic/_generate.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-echo "<string>" > temp1.txt
-echo "</string>" > temp2.txt
+# create script tag files to concat with plain JS in _style.js
+echo "<script>" > temp1.txt
+echo "</script>" > temp2.txt
+
+# for each file in both basic folders...
 for f in en-jap/* jap-en/*;
 do
+    # set output name to output dir + filename, and create the file / clear contents
     output="output/$f"
     mkdir -p $(dirname $output)
     touch $output
     > $output
 
-    cat temp1.txt _style.js temp2.txt >> $output
+    # concatenate file, style tags, and style script; remove temp files
+    cat $f temp1.txt _style.js temp2.txt >> $output
 done
 rm temp1.txt
 rm temp2.txt
