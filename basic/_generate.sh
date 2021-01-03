b9651c75ce3dde2bec8646154381be5bb5fb897c
Chris Dehner <topherdehner@gmail.com>
2021-01-02 22:38:06 -0500

    generate complete files for anki

--- basic/_generate.sh
+++ basic/_generate.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-for d in */;
+echo "<string>" > temp1.txt
+echo "</string>" > temp2.txt
+for f in en-jap/* jap-en/*;
 do
-    echo "$d"
+    output="output/$f"
+    mkdir -p $(dirname $output)
+    touch $output
+    > $output
+
+    cat temp1.txt _style.js temp2.txt >> $output
 done
+rm temp1.txt
+rm temp2.txt
